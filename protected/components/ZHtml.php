1b9252910066e880e7a1c8870f900803342b2531
nissimo <nissim@golemsystems.com>
2015-07-12 00:20:43 -0700

    test

--- protected/components/ZHtml.php
+++ protected/components/ZHtml.php
@@ -1,18 +1,22 @@
+
 class ZHtml extends CHtml
+
 {
         public static function enumDropDownList($model, $attribute, $htmlOptions=array())
             {
-            return CHtml::activeDropDownList( $model, $attribute, self::enumItem($model,  $attribute), $htmlOptions);
-            }
+         /* return CHtml::activeDropDownList( $model, $attribute, self::enumItem($model,  $attribute), $htmlOptions);*/
+
+            return array("zevel")
+}
 
         public static function enumItem($model,$attribute) {
             $attr=$attribute;
             self::resolveName($model,$attr);
             preg_match('/\((.*)\)/',$model->tableSchema->columns[$attr]->dbType,$matches);
-            foreach(explode("','", $matches[1]) as $value) {
-            $value=str_replace("'",null,$value);
-            $values[$value]=Yii::t('enumItem',$value);
-            }
+                foreach(explode("','", $matches[1]) as $value) {
+                $value=str_replace("'",null,$value);
+                $values[$value]=Yii::t('enumItem',$value);
+                }
             return $values;
             }
 }
